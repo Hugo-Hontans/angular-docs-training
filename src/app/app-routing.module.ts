78c980201a458e85b598bc9a8550c19350043c0b
Hugo Hontans <hugohontans@gmail.com>
2020-09-15 17:57:52 +0200

    change home page and init routing with first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'component-interaction', component: ComponentInteractionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
