2e1aa99be784af8c8417c61bfbd4e9c2fa1ea621
Hugo Hontans <hugohontans@gmail.com>
2020-09-26 13:15:38 +0200

    test child local variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 import { HeroChildComponent } from './component-interaction/hero-child/hero-child.component';
 import { HeroParentComponent } from './component-interaction/hero-parent/hero-parent.component';
+import { CountdownTimerComponent } from './component-interaction/countdown-timer/countdown-timer.component';
+import { CountdownParentComponent } from './component-interaction/countdown-parent/countdown-parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentInteractionComponent,
     HeroChildComponent,
-    HeroParentComponent
+    HeroParentComponent,
+    CountdownTimerComponent,
+    CountdownParentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component-interaction/countdown-parent/countdown-parent.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-parent',
+  templateUrl: './countdown-parent.component.html',
+  styleUrls: ['./countdown-parent.component.scss']
+})
+export class CountdownParentComponent {
+}
